4464b13432d2337a11d45c4dd21c7d30e36d6f39
vishalshubham <vishal.shubham@gmail.com>
2015-09-15 14:00:00 -0400

    MVC demo with MVC.jsp as View and Controller servlet as controller

---
+++ src/com/java/servlet/Controller.java
@@ -0,0 +1,51 @@
+package com.java.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Controller
+ */
+public class Controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Controller() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String page = request.getParameter("page");
+		
+		String gotopage = null;
+		
+		if(page.equals("login")){
+			gotopage = "/login.jsp";
+		}
+		else if(page.equals("about")){
+			gotopage = "/about.jsp";
+		}
+		else{
+			gotopage = "/error.jsp";
+		}
+		
+		getServletContext().getRequestDispatcher(gotopage).forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
