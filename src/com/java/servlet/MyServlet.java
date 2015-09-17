d578464233738043983ec3959116470e98dae69b
vishalshubham <vishal.shubham@gmail.com>
2015-09-16 21:08:13 -0400

    first commit

---
+++ src/com/java/servlet/MyServlet.java
@@ -0,0 +1,45 @@
+package com.java.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String para1 = request.getParameter("id");
+		
+		
+		String str = Some.getTime();
+		PrintWriter writer = response.getWriter();
+		writer.println("Write 1: " + str);
+		writer.println("Write 2: " + para1 + " Data coming from URL parameters");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ src/com/java/servlet/Some.java
@@ -0,0 +1,9 @@
+package com.java.servlet;
+
+import java.util.Date;
+
+public class Some {
+	public static String getTime(){
+		return "Returned from a class method directly";
+	}
+}
